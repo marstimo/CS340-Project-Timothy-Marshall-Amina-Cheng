f9cfdbc19d2f62435fd5926157c536874810b8ef
marstimo <marstimo@oregonstate.edu>
2026-02-12 15:09:44 -0800

    sets

--- DML.sql
+++ DML.sql
@@ -30,3 +30,25 @@ WHERE customerID=@customerID;
 ------------------------
 ----------Sets----------
 ------------------------
+
+-- Browse sets
+SELECT setID, name, description, releaseDate
+FROM Sets
+ORDER BY releaseDate DESC, name;
+
+-- Add set
+INSERT INTO Sets (name, description, releaseDate)
+VALUES (@name, @description, @releaseDate);
+
+-- Update set
+UPDATE Sets
+SET name=@name, description=@description, releaseDate=@releaseDate
+WHERE setID=@setID;
+
+-- Delete set
+DELETE FROM Sets
+WHERE setID=@setID;
+
+----------------------------
+----------Products----------
+----------------------------
