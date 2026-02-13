af4abf9e381fe83b6d86da86b68c5f165f47f14f
marstimo <marstimo@oregonstate.edu>
2026-02-12 22:05:13 -0800

    fix

--- server/database/db-connector.js
+++ server/database/db-connector.js
@@ -4,16 +4,10 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit   : 10,
     host              : 'classmysql.engr.oregonstate.edu',
-<<<<<<< HEAD
     user              : 'cs340_marstimo',
     password          : 'SjjPxu1E27g2',
     database          : 'cs340_marstimo'
 }).promise();
-=======
-    user              : 'cs340_chengam',
-    password          : 8525,
-    database          : 'cs340_chengam'
-}).promise(); // This makes it so we can use async / await rather than callbacks
->>>>>>> a61dfa2b98d15b16ef1c39fe329a5428a7845469
+
 
 module.exports = pool;
