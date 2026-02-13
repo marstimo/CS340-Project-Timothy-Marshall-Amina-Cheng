a95f998e11a09f70dcb48a50441e748ecd4058c1
marstimo <marstimo@oregonstate.edu>
2026-02-12 21:35:59 -0800

    database connected

--- server/database/db-connector.js
+++ server/database/db-connector.js
@@ -1,15 +1,12 @@
-// Get an instance of mysql we can use in the app
 let mysql = require('mysql2')
 
-// Create a 'connection pool' using the provided credentials
 const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit   : 10,
     host              : 'classmysql.engr.oregonstate.edu',
-    user              : 'cs340_[your_onid]',
-    password          : '[your_db_password]',
-    database          : 'cs340_[your_onid]'
-}).promise(); // This makes it so we can use async / await rather than callbacks
+    user              : 'cs340_marstimo',
+    password          : 'SjjPxu1E27g2',
+    database          : 'cs340_marstimo'
+}).promise();
 
-// Export it for use in our application
 module.exports = pool;
