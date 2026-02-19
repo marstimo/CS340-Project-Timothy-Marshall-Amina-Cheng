dd5e0245e7f340e98e36201aa3199a9dc823e123
Amina Cheng <chengam@oregonstate.edu>
2026-02-18 22:47:29 -0800

    Added password for testing

--- server/database/db-connector.js
+++ server/database/db-connector.js
@@ -4,9 +4,12 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit   : 10,
     host              : 'classmysql.engr.oregonstate.edu',
-    user              : 'cs340_marstimo',
-    password          : 'SjjPxu1E27g2',
-    database          : 'cs340_marstimo'
+    // user              : 'cs340_marstimo',
+    // password          : 'SjjPxu1E27g2',
+    // database          : 'cs340_marstimo'
+    user              : 'cs340_chengam',
+    password          : '8525',
+    database          : 'cs340_chengam'
 }).promise();
 
 
