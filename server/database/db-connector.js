47b456e555ea0b4e0f125f06d6118f711b1e5c30
Amina Cheng <chengam@oregonstate.edu>
2026-02-18 22:49:17 -0800

    Removing the password change

--- server/database/db-connector.js
+++ server/database/db-connector.js
@@ -4,12 +4,10 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit   : 10,
     host              : 'classmysql.engr.oregonstate.edu',
-    // user              : 'cs340_marstimo',
-    // password          : 'SjjPxu1E27g2',
-    // database          : 'cs340_marstimo'
-    user              : 'cs340_chengam',
-    password          : '8525',
-    database          : 'cs340_chengam'
+    user              : 'cs340_marstimo',
+    password          : 'SjjPxu1E27g2',
+    database          : 'cs340_marstimo'
+
 }).promise();
 
 
